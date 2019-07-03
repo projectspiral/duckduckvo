2323e7c735ea61cda0340ae94a30fa41e0157a82
svenvdvoort <svenvdvoort@protonmail.com>
2019-07-03 11:03:35 +0200

    Add deploy script

---
+++ deploy-website.sh
@@ -0,0 +1,25 @@
+# Exit if any errors occur
+set -e
+
+if [ -z $1 ]; then
+  echo "Please enter an directory to deploy to.";
+  exit 1;
+fi
+
+if [ ! -d $1 ]; then
+  echo "$1 is not a directory.";
+  exit 1;
+fi
+
+gitdir=$(realpath $(dirname $0));
+targetdir=$(realpath $1);
+
+echo "Deploying to: $(realpath $1)";
+
+# Make sure that we are in our own directory to execute git commands
+cd $gitdir;
+
+git checkout master
+git pull
+
+cp -r images/ *.html $targetdir
